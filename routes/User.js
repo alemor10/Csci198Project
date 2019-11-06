810daf3cda7b7af9a898269fdfbde72bb57f321e
Alejandro Morales <43051685+alemor10@users.noreply.github.com>
2019-11-05 18:07:07 -0800

    login and register finally work

--- routes/User.js
+++ routes/User.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const key = require('../config/keys')
 
+const key = require('../config/keys')
 const User = require('../models/users')
+const saltRounds = 10;
 /**
  * @route POST /users/register
  * @desc Register the User
@@ -53,12 +54,15 @@ router.post('/register', (req, res) => {
       password,
   });
   // Hash the password
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash("password", salt, function(err, hash) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
       // Store hash in your password DB.
       if (err) throw err;
       newUser.password = hash;
       console.log(newUser.username, newUser.password)
+      bcrypt.compare(req.body.password, newUser.password, function(err, res) {
+        // res === true
+        console.log(res)
+      });
       newUser.save().then(user => {
           return res.status(201).json({
               success: true,
@@ -66,7 +70,6 @@ router.post('/register', (req, res) => {
           });
       });
     });
-  });
 }catch(err) {
     throw err
 }
@@ -91,33 +94,37 @@ router.post('/login', (req, res) => {
             });
         }
         // If there is user we are now going to compare the password
-        bcrypt.compare(req.body.password, user.password,(err,data) => {
-            if (data == true) {
+        bcrypt.compare(req.body.password, user.password,function(err,data) {
+        console.log(data)
+        console.log(req.body.password , user.password)
+            if (data==true) {
                 // User's password is correct and we need to send the JSON Token for that user
                 const payload = {
-                    _id: user._id,
-                    username: user.username,
-                    name: user.name,
-                    email: user.email
+                  _id: user._id,
+                  username: user.username,
+                  name: user.name,
+                  email: user.email
                 }
                 jwt.sign(payload, key, {
-                    expiresIn: 604800
-                }, (err, token) => {
-                    res.status(200).json({
-                        success: true,
-                        token: `Bearer ${token}`,
-                        user: user,
-                        msg: "Hurry! You are now logged in."
-                    });
+                  expiresIn: 604800
+                }, 
+                (err, token) => {    
+                  res.status(200).json({
+                    success: true,
+                    token: `Bearer ${token}`,
+                    user: user,
+                    msg: "Hurry! You are now logged in."
+                  });
                 })
-            } else {
+            } 
+            else {
                 return res.status(404).json({
-                    msg: "Incorrect password.",
-                    success: false
+                  msg: "Incorrect password.",
+                  success: false    
                 });
             }
         })
-    });
+  });
 });
 
 
